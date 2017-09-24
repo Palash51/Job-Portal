4648d8b70b911df185cfe711ed7eb63b5138dfd4
Palash Patidar <palashpatidar08@gmail.com>
2017-09-24 11:06:06 +0530

    Added a Procfile.

--- recruitment/settings.py
+++ recruitment/settings.py
@@ -103,4 +103,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
